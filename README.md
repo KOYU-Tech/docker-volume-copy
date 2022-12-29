29b70f33387e427cc54d9889ce53766489c2d175
konstantin <adamasantares@gmail.com>
2022-12-29 22:23:59 +0600

    poop this commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# docker-volume-copy
-Bash script that allows to make a copy of exists valume to new one with a new name
+# Docker Volume Copy
+
+Bash script that allows to make a copy of exists volume to new one with a new name.
+
+**Step 1**
+
+Copy script to where you want use it
+
+```
+$ curl https://raw.githubusercontent.com/KOYU-Tech/docker-volume-copy/main/dvc.sh -o dvc.sh && chmod +x dvc.sh
+```
+
+**Step 2**
+
+Check the correct name of exists volume by ```$ docker volume ls```
+
+**Step 3**
+
+Imagine a new volume name and use it with script
+
+```
+$ ./dvc.sh old-volume-name new-volume-name-that-better-than-previous
+```
+
+Press "Enter" and wait.
+
+**Step 4**
+
+Go back to Github and give us a star 😅
---
+++ dvc.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Koyu.Tech @adamasantares
+
+if (( $# < 2 )); then
+    echo ""
+    echo "No arguments provided"
+    echo "Use command example:"
+    echo "docker-cv.sh OLD_VOLUME_NAME NEW_VOLUME_NAME"
+    echo ""
+    exit 1
+fi
+
+OLD_VOLUME_NAME="$1"
+NEW_VOLUME_NAME="$2"
+
+echo "== From '$OLD_VOLUME_NAME' to '$NEW_VOLUME_NAME' =="
+
+function isVolumeExists {
+    local isOldExists=$(docker volume inspect "$1" 2>/dev/null | grep '"Name":')
+    local isOldExists=${isOldExists#*'"Name": "'}
+    local isOldExists=${isOldExists%'",'}
+    local isOldExists=${isOldExists##*( )}
+    if [[ "$isOldExists" == "$1" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+
+# check if old volume exists
+isVolumeExists ${OLD_VOLUME_NAME}
+if [[ "$?" -eq 0 ]]; then
+    echo "Volume $OLD_VOLUME_NAME doesn't exist"
+    exit 2
+fi
+
+# check if new volume exists
+isVolumeExists ${NEW_VOLUME_NAME}
+if [[ "$?" -eq 0 ]]; then
+    echo "creating '$NEW_VOLUME_NAME' ..."
+    docker volume create ${NEW_VOLUME_NAME} 2>/dev/null 1>/dev/null
+    isVolumeExists ${NEW_VOLUME_NAME}
+    if [[ "$?" -eq 0 ]]; then
+        echo "Cannot create new volume"
+        exit 3
+    else
+        echo "OK"
+    fi
+fi
+
+# most important part, data migration
+docker run --rm --volume ${OLD_VOLUME_NAME}:/source --volume ${NEW_VOLUME_NAME}:/destination ubuntu:latest bash -c "echo 'copying volume ...'; cp -R /source/* /destination/"
+
+if [[ "$?" -eq 0 ]]; then
+    echo "Done successfuly 🎉"
+else
+    echo "Some error occured 😭"
+fi
